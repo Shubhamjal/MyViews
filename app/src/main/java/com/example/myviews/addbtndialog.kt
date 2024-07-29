2a91d00712dca161426e7e59ff0be378709eeef7
Shubham Kumar <appscicseshubham22@gmail.com>
2024-07-29 10:55:29 +0530

    spinner implemented

--- app/src/main/java/com/example/myviews/addbtndialog.kt
+++ app/src/main/java/com/example/myviews/addbtndialog.kt
@@ -1,37 +1,20 @@
 package com.example.myviews
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.myviews.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
-
+class addbtndialog : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        binding=ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_addbtndialog)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        binding.ListViewbtn.setOnClickListener {
-            var intent= Intent(this,ListViewActivity::class.java)
-            startActivity(intent)
-        }
-        binding.SpinnerViewbtn.setOnClickListener {
-            var intent= Intent(this,SpinnerViewActivity::class.java)
-            startActivity(intent)
-        }
-
     }
 }
